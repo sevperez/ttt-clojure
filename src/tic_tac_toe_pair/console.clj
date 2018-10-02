b6b234a48c8c4f0053cc5a9343a6b1a4c537ef9d
Severin Perez <severinperez@gmail.com>
2018-10-02 16:07:42 -0700

    Enhance board print out

--- src/tic_tac_toe_pair/console.clj
+++ src/tic_tac_toe_pair/console.clj
@@ -3,17 +3,17 @@
   (:gen-class))
 
 (def board-shape 
-  (str "     |     |     \n"
-       "  0  |  1  |  2  \n"
-       "     |     |     \n"
-       "-----------------\n"
-       "     |     |     \n"
-       "  3  |  4  |  5  \n"
-       "     |     |     \n"
-       "-----------------\n"
-       "     |     |     \n"
-       "  6  |  7  |  8  \n"
-       "     |     |     "))
+  (str "          |     |     \n"
+       "       0  |  1  |  2  \n"
+       "          |     |     \n"
+       "     -----------------\n"
+       "          |     |     \n"
+       "       3  |  4  |  5  \n"
+       "          |     |     \n"
+       "     -----------------\n"
+       "          |     |     \n"
+       "       6  |  7  |  8  \n"
+       "          |     |     \n"))
 
 (defn keyword-to-token [keyword] (s/upper-case (name keyword)))
 
@@ -38,6 +38,56 @@
 (defn draw-board [board] 
   (println (build-board board)))
 
-(defn get-move-location [] 
-  (println "Enter a move:")
-  (Integer/parseInt (read-line)))
+(defn clear-terminal []
+  (do 
+    (print "\u001b[2J")
+    (print "\u001B[0;0f")))
+
+(defn draw-header []
+  (println "---------------------------\nTic Tac Toe\n---------------------------"))
+
+(defn draw-player-info []
+  (println "Player 1: X     Player 2: O\n"))
+
+(defn draw-footer []
+  (println "---------------------------\n"))
+
+(defn draw-main
+  ([game]
+    (do
+      (clear-terminal)
+      (draw-header)
+      (draw-player-info)
+      (draw-board (:board game))
+      (draw-footer)))
+  ([game message]
+    (do
+      (clear-terminal)
+      (draw-header)
+      (draw-player-info)
+      (draw-board (:board game))
+      (println message)
+      (draw-footer))))
+
+(defn build-current-player-string [game] 
+  (if (= :player-1-token (:current-token game))
+    "Player 1's move!"
+    "Player 2's move!"))
+
+(defn get-move-location [game] 
+  (do
+    (draw-main game (build-current-player-string game))
+    (println "Choose a move: "))
+  (dec (try 
+    (Integer/parseInt (read-line))
+    (catch NumberFormatException e (get-move-location game)))))
+
+
+; defn game msg="choose a move"
+;   try
+;     printline msg
+;     parseint (readline)
+;     if notvalidint
+;       throw custom error
+;   catch
+;     call self (game "error message")
--- test/tic_tac_toe_pair/console_test.clj
+++ test/tic_tac_toe_pair/console_test.clj
@@ -5,35 +5,56 @@
 (deftest draw-board-test
   (testing "it draws an empty board to the console")
     (is (= 
-      (str "     |     |     \n"
-           "     |     |     \n"
-           "     |     |     \n"
-           "-----------------\n"
-           "     |     |     \n"
-           "     |     |     \n"
-           "     |     |     \n"
-           "-----------------\n"
-           "     |     |     \n"
-           "     |     |     \n"
-           "     |     |     \n")
+      (str "          |     |     \n"
+           "          |     |     \n"
+           "          |     |     \n"
+           "     -----------------\n"
+           "          |     |     \n"
+           "          |     |     \n"
+           "          |     |     \n"
+           "     -----------------\n"
+           "          |     |     \n"
+           "          |     |     \n"
+           "          |     |     \n\n")
       (with-out-str
         (draw-board [nil nil nil nil nil nil nil nil nil]))))
   (testing "it draws a populated board to the console")
     (is (= 
-      (str "     |     |     \n"
-           "     |  X  |     \n"
-           "     |     |     \n"
-           "-----------------\n"
-           "     |     |     \n"
-           "  X  |     |     \n"
-           "     |     |     \n"
-           "-----------------\n"
-           "     |     |     \n"
-           "     |  O  |     \n"
-           "     |     |     \n")
+      (str "          |     |     \n"
+           "          |  X  |     \n"
+           "          |     |     \n"
+           "     -----------------\n"
+           "          |     |     \n"
+           "       X  |     |     \n"
+           "          |     |     \n"
+           "     -----------------\n"
+           "          |     |     \n"
+           "          |  O  |     \n"
+           "          |     |     \n\n")
       (with-out-str
         (draw-board [nil :x nil :x nil nil nil :o nil])))))
 
 (deftest get-move-location-test
   (testing "it returns an integer value")
-    (is (= 4 (with-in-str "4" (get-move-location)))))
+    (with-out-str (is 
+      (= 4 (with-in-str "5"
+        (get-move-location
+          {:current-token :player-2-token
+            :player-1-token :x
+            :player-2-token :o
+            :board [:x :o nil :x :o nil :x nil nil]}))))))
+
+(deftest build-current-player-string-test
+  (testing "it returns a string with the current player's name and token")
+    (is (= "Player 1's move!"
+      (build-current-player-string
+        {:current-token :player-1-token
+         :player-1-token :x
+         :player-2-token :o
+         :board [:x :o nil :x :o nil :x nil nil]})))
+    (is (= "Player 2's move!"
+      (build-current-player-string
+        {:current-token :player-2-token
+         :player-1-token :x
+         :player-2-token :o
+         :board [:x :o nil :x :o nil :x nil nil]}))))
