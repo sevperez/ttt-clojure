c730e6c83657b2acf4a0adde9e6e756e49cfb280
Severin Perez <severinperez@gmail.com>
2018-10-03 15:26:19 -0700

    Add input validation

--- src/tic_tac_toe_pair/console.clj
+++ src/tic_tac_toe_pair/console.clj
@@ -1,5 +1,6 @@
 (ns tic-tac-toe-pair.console 
-  (:require [clojure.string :as s])
+  (:require [clojure.string :as s]
+            [tic-tac-toe-pair.board :refer :all])
   (:gen-class))
 
 (def board-shape 
@@ -74,13 +75,40 @@
     "Player 1's move!"
     "Player 2's move!"))
 
-(defn get-move-location [game] 
-  (do
-    (draw-main game (build-current-player-string game))
-    (println "Choose a move: "))
-  (dec (try 
-    (Integer/parseInt (read-line))
-    (catch NumberFormatException e (get-move-location game)))))
+(defn read-move-input [game] 
+  (let [input (dec (Integer/parseInt (read-line)))]
+    (if (is-move-valid? (:board game) input) 
+      input
+      (throw (ex-info "You've entered an invalid move." {})))))
+
+(defn get-available-indices [board]
+  (loop [index  0
+         result []]
+    (if (>= index (count board))
+      result
+      (if (nil? (get board index))
+        (recur (inc index) (conj result (inc index)))
+        (recur (inc index) result)))))
+
+(defn build-choose-move-string [game]
+  (str "Choose a move: ("
+    (s/join ", " (get-available-indices (:board game))) ")"))
+
+(defn get-move-location
+  ([game]
+    (get-move-location game (build-choose-move-string game)))
+  ([game message]
+    (do
+      (draw-main game (build-current-player-string game))
+      (println message))
+    (try 
+      (read-move-input game)
+      (catch NumberFormatException e 
+        (get-move-location game 
+          (str "Invalid entry. " (build-choose-move-string game))))
+      (catch clojure.lang.ExceptionInfo e
+        (get-move-location game
+          (str "Unavailable entry. " (build-choose-move-string game)))))))
 
 
 ; defn game msg="choose a move"
--- test/tic_tac_toe_pair/console_test.clj
+++ test/tic_tac_toe_pair/console_test.clj
@@ -35,14 +35,45 @@
         (draw-board [nil :x nil :x nil nil nil :o nil])))))
 
 (deftest get-move-location-test
-  (testing "it returns an integer value")
+  (testing "it returns an integer value one less than valid input")
     (with-out-str (is 
       (= 4 (with-in-str "5"
         (get-move-location
           {:current-token :player-2-token
             :player-1-token :x
             :player-2-token :o
-            :board [:x :o nil :x :o nil :x nil nil]}))))))
+            :board [:x nil nil nil nil nil nil nil nil]})))))
+  (testing "it continues requesting input until it's valid")
+    (with-out-str (is 
+      (= 4 (with-in-str "20\n5"
+        (get-move-location
+          {:current-token :player-2-token
+            :player-1-token :x
+            :player-2-token :o
+            :board [:x nil nil nil nil nil nil nil nil]}))))))
+
+(deftest read-move-input-test
+  (testing "it returns an an integer if input is valid")
+    (is (= 5 
+      (with-in-str "6" 
+        (read-move-input {:current-token :player-2-token
+                          :player-1-token :x
+                          :player-2-token :o
+                          :board [:x nil nil nil nil nil nil nil nil]}))))
+  (testing "it throws a NumberFormatException if input is not integer")
+    (is (thrown? NumberFormatException 
+      (with-in-str "a" 
+        (read-move-input {:current-token :player-2-token
+                          :player-1-token :x
+                          :player-2-token :o
+                          :board [:x nil nil nil nil nil nil nil nil]}))))
+  (testing "it throws a custom exception if input is integer but invalid")
+    (is (thrown? clojure.lang.ExceptionInfo
+      (with-in-str "1"
+        (read-move-input {:current-token :player-2-token
+                          :player-1-token :x
+                          :player-2-token :o
+                          :board [:x nil nil nil nil nil nil nil nil]})))))
 
 (deftest build-current-player-string-test
   (testing "it returns a string with the current player's name and token")
@@ -58,3 +89,19 @@
          :player-1-token :x
          :player-2-token :o
          :board [:x :o nil :x :o nil :x nil nil]}))))
+
+(deftest build-choose-move-string-test
+  (testing "it returns a string listing all 9 spaces available on empty board")
+    (is (= "Choose a move: (1, 2, 3, 4, 5, 6, 7, 8, 9)"
+      (build-choose-move-string
+        {:current-token :player-2-token
+         :player-1-token :x
+         :player-2-token :o
+         :board [nil nil nil nil nil nil nil nil nil]})))
+  (testing "it returns a string listing only available spaces")
+    (is (= "Choose a move: (3, 6, 8, 9)"
+      (build-choose-move-string
+        {:current-token :player-2-token
+         :player-1-token :x
+         :player-2-token :o
+         :board [:x :o nil :x :x nil :o nil nil]}))))
